677b6f0365004e9db3f4ecc7f5f4064ec1be359b
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-06-30 14:02:59 +0300

    [121] [Time Beats: 61.87%] [Memory Beats: 50.76%] - LeetPush

--- JS/Best-Time-to-Buy-and-Sell-Stock.js
+++ JS/Best-Time-to-Buy-and-Sell-Stock.js
@@ -17,29 +17,22 @@ var maxProfit = function(prices) {
     // i shouldn't update keep after with a price after the selling price, hmmm
     // should i push the profit of (old-sell - old-keep) to an array, and then reset the sell and assign the new value to keep?
     // AFTER FIRST SUBMISSION => there is no need for a profits ARRAY, we only want a maxProfit variable to reassign
-    let keep = prices[0];
-    let sell = -1;
-    let maxProfit = null;
 
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < keep) {
-            if (sell !== -1) {
-                maxProfit = Math.max(maxProfit, sell - keep);
-            }
-            keep = prices[i];
-            sell = -1;
-        } else if (prices[i] > sell) {
-            sell = prices[i];
+    // AFTER SECOND SUBMIT => according to: https://www.youtube.com/watch?v=1pkOgXD63yU
+    // why do we need to only conditionally change the sell ponter when we could always move it to the right
+    // and only measure and update the keep when it's not profitable?
+    let keep = 0;
+    let sell = 1;
+    let maxProfit = 0;
+
+    while (sell < prices.length) {
+        if (prices[keep] < prices[sell]) { // it's profitable to sell
+            maxProfit = Math.max(maxProfit, prices[sell] - prices[keep]);
+        } else { // sell is lower than keep, so keep becomes sell as the lowest price to buy
+            keep = sell;
         }
+        sell = sell + 1;
     }
-
-    // in case of only one profit there is
-    if (sell !== -1) {
-        maxProfit = Math.max(maxProfit, sell - keep);
-    }
-
-    // get the maximum profit after multiple keeps and sells
-    if (maxProfit === null) return 0; // no profit could be achieved
 
     return maxProfit;
 };
