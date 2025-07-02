791bafc1f6160f1ae6452a72e2ee776ca99d2863
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-02 12:34:36 +0300

    [371] [Time Beats: 100.00%] [Memory Beats: 7.17%] - LeetPush

--- JS/Sum-of-Two-Integers.js
+++ JS/Sum-of-Two-Integers.js
@@ -10,7 +10,8 @@ var getSum = function(a, b) {
     // ith bit is applied to i+1th, right? so 1 << 0011 is 0110
     // now do XOR with that and then AND and see if this creates carries as well, if yes keep going, if no we're done
     // now if say b is negative, we got a fucking two's complement in our hand where -b is ~b+1 so it invloves addition
-    // but lucky for us we just figured out addition!
+    // but good news is, JavaScript's bitwise ops natively handle negatives in two's complement, so our add function 
+    // already takes care of them. No extra conversion needed
     function add(a, b) {
         while (b !== 0) {
             const carry = (a & b) << 1;
@@ -18,16 +19,6 @@ var getSum = function(a, b) {
             b = carry;
         }
         return a;
-    }
-
-    if (a < 0) {
-        a *= -1;
-        a = add(~a, 1);
-    }
-
-    if (b < 0) {
-        b *= -1;
-        b = add(~b, 1);
     }
 
     return add(a, b);
