3716627655eb914935e2ceb7c54f2ed4e6ab0658
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-08 16:15:07 +0300

    [206] [Time Beats: 100.00%] [Memory Beats: 6.26%] - LeetPush

--- JS/Reverse-Linked-List.js
+++ JS/Reverse-Linked-List.js
@@ -13,17 +13,29 @@ var reverseList = function (head) {
     // for each curr, we do curr.next = prev to reverese the arrow direction, and then move prev to curr, curr to next, and next= next.next
     // what if we have less than three nodes? if 0 or 1, just return head, if two, our approach hold if we we have
     // prev = null (before head), curr = head, and next = head.next
+    // if (!head || !head.next) return head;
+    // let prev = null;
+    // let curr = head;
+    // let next = head.next;
+
+    // while (curr) {
+    //     curr.next = prev;
+    //     prev = curr;
+    //     curr = next;
+    //     if (next) next = next.next; // last node to flip will be curr, so next would be null
+    // }
+
+    // return prev;
+
+    // ---------------------------------------------------------------------------------------------
+
+    // recursive approach would be, if i have one node, then it's already reveresed, and if i have two nodes x and y
+    // then to reverse them we do, x.next = null, y.next = x
     if (!head || !head.next) return head;
-    let prev = null;
-    let curr = head;
-    let next = head.next;
 
-    while (curr) {
-        curr.next = prev;
-        prev = curr;
-        curr = next;
-        if (next) next = next.next; // last node to flip will be curr, so next would be null
-    }
+    const newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
 
-    return prev;
+    return newHead;
 };
