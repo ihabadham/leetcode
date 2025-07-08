580fc2ff58f23d5ed7e259a5660fa49e51390a54
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-08 15:31:23 +0300

    [206] [Time Beats: 100.00%] [Memory Beats: 58.43%] - LeetPush

---
+++ JS/Reverse-Linked-List.js
@@ -0,0 +1,29 @@
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    // iterative approach would be to loop over the list with three pointers: prev, curr, and next
+    // for each curr, we do curr.next = prev to reverese the arrow direction, and then move prev to curr, curr to next, and next= next.next
+    // what if we have less than three nodes? if 0 or 1, just return head, if two, our approach hold if we we have
+    // prev = null (before head), curr = head, and next = head.next
+    if (!head || !head.next) return head;
+    let prev = null;
+    let curr = head;
+    let next = head.next;
+
+    while (curr) {
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        if (next) next = next.next; // last node to flip will be curr, so next would be null
+    }
+
+    return prev;
+};
