45785769b72850014a3fad4476fd6f40d7b38b8c
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-09 21:56:04 +0300

    [141] [Time Beats: 96.84%] [Memory Beats: 71.06%] - LeetPush

---
+++ JS/Linked-List-Cycle.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    // ok i looked quicly to the answer yesterday, let's see if i remember it
+    // so basically to detect if there's a cycle in a given list, we take two pointers, one is slowly moving and one is moving faster
+    // slow means traversing node by node, fast means traversing 2 nodes at a time, so if there was a cycle, eventually slow
+    // pointer will reach the fast one and vice versa.
+    if (!head || !head.next) return false;
+
+    let slow = head;
+    let fast = head.next;
+
+    while (fast && fast.next) {
+        if (slow === fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return false
+};
