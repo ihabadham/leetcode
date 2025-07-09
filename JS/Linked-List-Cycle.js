f4b999d6db1f66e3bc34866d4b517013909cf502
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-09 22:00:05 +0300

    [141] [Time Beats: 64.45%] [Memory Beats: 14.11%] - LeetPush

--- JS/Linked-List-Cycle.js
+++ JS/Linked-List-Cycle.js
@@ -15,15 +15,19 @@ var hasCycle = function(head) {
     // so basically to detect if there's a cycle in a given list, we take two pointers, one is slowly moving and one is moving faster
     // slow means traversing node by node, fast means traversing 2 nodes at a time, so if there was a cycle, eventually slow
     // pointer will reach the fast one and vice versa.
+    // AFTER SUBMIT: apparently we're doing some redundant ops because we're not starting both pointers from same start
+    // and i made them that way to avoid meeting this condition if (slow === fast) return true; at first thing
+    // before any movements happen. previous solution was correct, but somehow a bit redundant. 
+    // loop up Floyd's Cycle-Finding Algorithm
     if (!head || !head.next) return false;
 
     let slow = head;
-    let fast = head.next;
+    let fast = head;
 
     while (fast && fast.next) {
-        if (slow === fast) return true;
         slow = slow.next;
         fast = fast.next.next;
+        if (slow === fast) return true;
     }
 
     return false
