2b4fe6ca0c61e36ca35583e0d4daedb4d2b8c201
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-10 13:40:02 +0300

    [1394] [Time Beats: 38.79%] [Memory Beats: 77.07%] - LeetPush

--- JS/Find-Lucky-Integer-in-an-Array.js
+++ JS/Find-Lucky-Integer-in-an-Array.js
@@ -2,22 +2,52 @@
  * @param {number[]} arr
  * @return {number}
  */
-var findLucky = function(arr) {
+var findLucky = function (arr) {
     // i can't believe i found a problem this easy on leetcode
     // classical freq array. we create an array of length 501 (upper bound of maximum possible value of arr[i]), fill it with 0's
     // iterate over arr, do arr[i]++ until the end
     // come back to freq, see if any item is freq[i] === i, return max = Math.max(max, freq[i])
     // if no lucky number found return -1
-    let freq = new Array(501).fill(0);
-    let maxL = 0;
-    
-    for (const num of arr) freq[num]++;
+    // let freq = new Array(501).fill(0);
+    // let maxL = 0;
 
-    for (let i = 1; i < freq.length; i++) {
-        if (freq[i] === i) {
-            maxL = Math.max(maxL, freq[i]);
+    // for (const num of arr) freq[num]++;
+
+    // for (let i = 1; i < freq.length; i++) {
+    //     if (freq[i] === i) {
+    //         maxL = Math.max(maxL, freq[i]);
+    //     }
+    // }
+
+    // return maxL || -1;
+
+    // --------------------------------------------------------------------------------------------
+
+    // another appraoch would be counting, without using a freq array. 
+    // since array element order doesn't count, we can just sort the array descendingly, move two pointers l and r
+    // and count the similar items and update max
+    // AFTER WRONG SUBMIT: what a bad early return condition i have made :(
+    arr.sort((a, b) => b - a);
+    let l = 0;
+    let r = 1;
+    let count = 1;
+
+    while (r < arr.length) {
+        if (arr[r] !== arr[l]) {
+            if (count === arr[l]) {
+                return count; // we don't have to keep looking, this is lucky, and because of the sort, this is max
+            }
+
+            l = r;
+            count = 1;
+        } else {
+            count = count + 1;
         }
+
+        r = r + 1;
     }
 
-    return maxL || -1;
+    if (count === arr[l]) return count;
+
+    return -1;
 };
