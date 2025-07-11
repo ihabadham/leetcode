8a58be0865f5be4b1cedbda3bcdd5a15711eec3a
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-11 20:08:15 +0300

    [168] [Time Beats: 100.00%] [Memory Beats: 38.40%] - LeetPush

---
+++ JS/Excel-Sheet-Column-Title.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+    // this is classical base conversion
+    // we basically convert from base 10 to base X by doing two ops iteratively
+    // we dvide our number by the base to get an integer result, and take modulo to get remainder
+    // repeat until division result is 0. answer is remainders bottom to top
+
+    const getCapitalLetter = (n) =>
+        String.fromCharCode("A".charCodeAt(0) + n);
+
+
+    let res = "";
+    let rem = 0;
+
+    while (columnNumber) {
+        columnNumber--; // this is because we're converting to base 26 1-indexed and not 0-indexed like normal conversions. we're essentially converting from 1-based to 0-based so we can apply the normal algorithm
+        rem = columnNumber % 26;
+        res += getCapitalLetter(rem);
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+
+    return res.split("").reverse().join("");
+};
