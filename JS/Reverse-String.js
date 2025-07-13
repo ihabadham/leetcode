ef914922f7020f1ab5f28df41c4dfc567bf311c3
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-13 17:56:44 +0300

    [344] [Time Beats: 29.09%] [Memory Beats: 43.14%] - LeetPush

---
+++ JS/Reverse-String.js
@@ -0,0 +1,15 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    // left and right pointers and swap
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        [s[l], s[r]] = [s[r], s[l]]
+        l++;
+        r--;
+    }
+};
