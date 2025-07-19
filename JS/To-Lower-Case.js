78cbb977c7cf06951dfe861ea66c8f83db02a52e
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-19 17:11:41 +0300

    [709] [Time Beats: 100.00%] [Memory Beats: 50.14%] - LeetPush

---
+++ JS/To-Lower-Case.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    // i know the ascii conversion way, but i don't give a damn
+    return s.toLowerCase();
+};
