000abbf015a1ebdfeca6dcfc2321edbfd2e0860a
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-19 17:17:27 +0300

    [709] [Time Beats: 13.36%] [Memory Beats: 19.11%] - LeetPush

--- JS/To-Lower-Case.js
+++ JS/To-Lower-Case.js
@@ -2,7 +2,27 @@
  * @param {string} s
  * @return {string}
  */
-var toLowerCase = function(s) {
+var toLowerCase = function (s) {
     // i know the ascii conversion way, but i don't give a damn
-    return s.toLowerCase();
+    // return s.toLowerCase();
+
+    // ok fuck it let's do it
+    function convertCharAtIndex(str, index) {
+        const char = str[index];
+        const charCode = char.charCodeAt(0);
+
+        if (charCode >= 65 && charCode <= 90) {
+            const lowercaseCharCode = charCode + 32;
+            const lowercaseChar = String.fromCharCode(lowercaseCharCode);
+            return str.substring(0, index) + lowercaseChar + str.substring(index + 1);
+        } else {
+            return str;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        s = convertCharAtIndex(s, i);
+    }
+
+    return s;
 };
