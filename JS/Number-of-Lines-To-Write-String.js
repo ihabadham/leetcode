66f7b64e70682181ade51a87c8b6ace0f2a6ea47
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-21 19:31:48 +0300

    [806] [Time Beats: 100.00%] [Memory Beats: 49.26%] - LeetPush

--- JS/Number-of-Lines-To-Write-String.js
+++ JS/Number-of-Lines-To-Write-String.js
@@ -6,14 +6,17 @@
 var numberOfLines = function (widths, s) {
     let lineCount = 1;
     let pixelCount = 0;
+    const aCharCode = 'a'.charCodeAt(0);
 
     for (const char of s) {
-        if ((pixelCount + widths[char.charCodeAt(0) - 'a'.charCodeAt(0)]) > 100) {
+        const charWidth = widths[char.charCodeAt(0) - aCharCode];
+
+        if ((pixelCount + charWidth) > 100) {
             lineCount++;
             pixelCount = 0;
         }
 
-        pixelCount += widths[char.charCodeAt(0) - 'a'.charCodeAt(0)];
+        pixelCount += charWidth;
     }
 
     return [lineCount, pixelCount];
