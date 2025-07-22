f0943081f5c83c283fabdf48c319cb3760f0ab68
Ihab Adham <71561048+ihabadham@users.noreply.github.com>
2025-07-22 15:13:52 +0300

    [796] [Time Beats: 100.00%] [Memory Beats: 56.42%] - LeetPush

--- JS/Rotate-String.js
+++ JS/Rotate-String.js
@@ -5,14 +5,28 @@
  */
 var rotateString = function (s, goal) {
     // 1. i could rotate s s.length times and each time compare it with goal
-    // 2. i i could iterate over s until i find the first character that matches the goal[0] rotate only then
-    const rotate = (str) => str.slice(1) + str.slice(0, 1);
+    // 2. i could iterate over s until i find the first character that matches the goal[0] rotate only then
+    // const rotate = (str) => str.slice(1) + str.slice(0, 1);
 
-    let n = s.length;
-    while (n > 0) {
-        if (s === goal) return true;
-        s = rotate(s);
-        n--;
+    // let n = s.length;
+    // while (n > 0) {
+    //     if (s === goal) return true;
+    //     s = rotate(s);
+    //     n--;
+    // }
+
+    // return false;
+
+    if (s.length !== goal.length) return false;
+    if (s === goal) return true;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === goal[0]) {
+            let rotatedS = s.slice(i) + s.slice(0, i);
+            if (rotatedS === goal) {
+                return true;
+            }
+        }
     }
 
     return false;
